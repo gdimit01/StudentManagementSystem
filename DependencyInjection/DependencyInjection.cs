d0cb56f62d802d85fc9e4c34213a564356cdcfed
Edgar Torres <edgartrs@gmail.com>
2024-10-25 00:14:35 -0500

    Fix DI name typo, fix StudentController not able to instantiate service

--- DependencyInjection/DependencyInjection.cs
+++ DependencyInjection/DependencyInjection.cs
@@ -4,13 +4,14 @@ using StudentManagementSystem.Services;
 
 namespace StudentManagementSystem.DependencyInjection;
 
-public static class DepdendencyInjection
+public static class DependencyInjection
 {
     public static void RegisterDependencies(this IServiceCollection services)
     {
         services.AddScoped<IStudentRepository, StudentRepository>();
         services.AddScoped<ITeacherRepository, TeacherRepository>();
         services.AddScoped<TeacherService>(); // Register TeacherService
+        services.AddScoped<StudentService>();
         services.AddScoped<AuthService>(); // Register AuthService
     }
 }
